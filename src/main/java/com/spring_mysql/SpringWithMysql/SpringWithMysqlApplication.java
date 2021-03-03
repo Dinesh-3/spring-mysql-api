5e56960e859c5c129c4f2187e1908226117be5ab
Dinesh I <dineshiyyandhurai@gmail.com>
2021-03-03 15:55:52 +0530

    First Commit

---
+++ src/main/java/com/spring_mysql/SpringWithMysql/SpringWithMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.spring_mysql.SpringWithMysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithMysqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_mysql/SpringWithMysql/SpringWithMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_mysql.SpringWithMysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
